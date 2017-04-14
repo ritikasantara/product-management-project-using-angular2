79e6452200ab6ec79bb56fb849716739c5db63fb
ritikasantara <rits.santara@gmail.com>
2017-04-14 21:04:04 +0530

    displayed products and their details

--- pm-angular2/src/app/app.component.ts
+++ pm-angular2/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hey folks!';
+  title: string;
+
+  constructor(){
+
+    this.title = "Bagzkart Product Management";
+  }
 }
--- pm-angular2/src/app/components/products/product-list/product-list.component.ts
+++ pm-angular2/src/app/components/products/product-list/product-list.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
 
-  constructor() { }
+  pageTitle: string;
+  imageWidth: number;
+  imageMargin: number;
+  products: any[];
 
-  ngOnInit() {
-  }
+  constructor() { 
+
+    this.pageTitle = "Product List";
+    this.imageWidth = 50;
+    this.imageMargin = 3;
+
+    this.products = [
+      {
+        productId : 1,
+        productName : "Louis Vuitton Tote Bag",
+        productCode : "abc-123",
+        releaseDate : "March 18, 2017",
+        description : "Tote Bag....lorem ipsom h",
+        price : 1099,
+        starRating : 3.9,
+        imageUrl : "http://uk.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-armand-open-tote-taurillon-leather-men-s-bags--M42683_PM2_Front%20view.jpg"
+      },
+
+      {
+        productId : 2,
+        productName : "Polyvore Satchel Bag",
+        productCode : "pqr-456",
+        releaseDate : "April 07, 2017",
+        description : "Satchel Bag....lorem ipsom h",
+        price : 1599,
+        starRating : 4.9,
+        imageUrl : "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=161416907"
+
+      },
+
+      {
+
+        productId : 3,
+        productName : "Butterflies Sling Bag",
+        productCode : "ghi-789",
+        releaseDate : "Feb 25, 2017",
+        description : "Sling Bag....lorem ipsom h",
+        price : 1089,
+        starRating : 5,
+        imageUrl : "https://n1.sdlcdn.com/imgs/a/0/p/Butterflies-Quilted-Beige-Cross-Body-SDL888771080-1-6ae5d.jpg"
+      },
+
+      {
+
+        productId : 4,
+        productName : "Hidesign Pouch Bag",
+        productCode : "def-678",
+        releaseDate : "March 28, 2017",
+        description : "Tote Bag....lorem ipsom h",
+        price : 1299,
+        starRating : 3.6,
+        imageUrl : "http://www.bodenimages.com/productimages/productmedium/17wsum_am278_tan.jpg"
+      }
+
+    ];
+
+ }
 
 }
+
