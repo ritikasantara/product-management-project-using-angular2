01f83a6f998979b5e476d769f3dcce8cc1a59ba6
ritikasantara <rits.santara@gmail.com>
2017-04-16 19:50:37 +0530

    added a custom product search filter pipe

--- pm-angular2/src/app/components/products/product-list/product-list.component.ts
+++ pm-angular2/src/app/components/products/product-list/product-list.component.ts
@@ -1,20 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
   pageTitle: string;
   imageWidth: number;
   imageMarginleft: number;
   imagePaddingleft: number;
   productTdpadding: number;
-  products: any[];
+  products: IProduct[];
   showImage: boolean;
-  listFilter:string;
+  listFilter: string;
 
   constructor() { 
 
@@ -27,50 +27,50 @@ export class ProductListComponent {
 
     this.products = [
       {
-        productId : 1,
-        productName : "Louis Vuitton Tote Bag",
-        productCode : "abc-123",
-        releaseDate : "March 18, 2017",
-        description : "Tote Bag....lorem ipsom h",
-        price : 1099,
-        starRating : 3.9,
-        imageUrl : "http://uk.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-armand-open-tote-taurillon-leather-men-s-bags--M42683_PM2_Front%20view.jpg"
+        "productId" : 1,
+        "productName" : "Louis Vuitton Tote Bag",
+        "productCode" : "abc-123",
+        "releaseDate" : "March 18, 2017",
+        "description" : "Tote Bag....lorem ipsom h",
+        "price" : 1099,
+        "starRating" : 3.9,
+        "imageUrl" : "http://uk.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-armand-open-tote-taurillon-leather-men-s-bags--M42683_PM2_Front%20view.jpg"
       },
 
       {
-        productId : 2,
-        productName : "Polyvore Satchel Bag",
-        productCode : "pqr-456",
-        releaseDate : "April 07, 2017",
-        description : "Satchel Bag....lorem ipsom h",
-        price : 1599,
-        starRating : 4.9,
-        imageUrl : "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=161416907"
+        "productId" : 2,
+        "productName" : "Polyvore Satchel Bag",
+        "productCode" : "pqr-456",
+        "releaseDate" : "April 07, 2017",
+        "description" : "Satchel Bag....lorem ipsom h",
+        "price" : 1599,
+        "starRating" : 4.9,
+        "imageUrl" : "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=161416907"
 
-      },
-
-      {
-
-        productId : 3,
-        productName : "Butterflies Sling Bag",
-        productCode : "ghi-789",
-        releaseDate : "Feb 25, 2017",
-        description : "Sling Bag....lorem ipsom h",
-        price : 1089,
-        starRating : 5,
-        imageUrl : "https://n1.sdlcdn.com/imgs/a/0/p/Butterflies-Quilted-Beige-Cross-Body-SDL888771080-1-6ae5d.jpg"
       },
 
       {
 
-        productId : 4,
-        productName : "Hidesign Pouch Bag",
-        productCode : "def-678",
-        releaseDate : "March 28, 2017",
-        description : "Tote Bag....lorem ipsom h",
-        price : 1299,
-        starRating : 3.6,
-        imageUrl : "http://www.bodenimages.com/productimages/productmedium/17wsum_am278_tan.jpg"
+        "productId" : 3,
+        "productName" : "Butterflies Sling Bag",
+        "productCode" : "ghi-789",
+        "releaseDate" : "Feb 25, 2017",
+        "description" : "Sling Bag....lorem ipsom h",
+        "price" : 1089,
+        "starRating" : 5,
+        "imageUrl" : "https://n1.sdlcdn.com/imgs/a/0/p/Butterflies-Quilted-Beige-Cross-Body-SDL888771080-1-6ae5d.jpg"
+      },
+
+      {
+
+        "productId" : 4,
+        "productName" : "Hidesign Pouch Bag",
+        "productCode" : "def-678",
+        "releaseDate" : "March 28, 2017",
+        "description": "Tote Bag....lorem ipsom h",
+        "price" : 1299,
+        "starRating" : 3.6,
+        "imageUrl" : "http://www.bodenimages.com/productimages/productmedium/17wsum_am278_tan.jpg"
       }
 
     ];
@@ -83,5 +83,24 @@ export class ProductListComponent {
 
     }
 
+    ngOnInit(): void{
+      console.log('OnInit initiated..');
+    }
+
+
+   
 }
+
+ export interface IProduct{
+
+      productId : number;
+      productName : string;
+      productCode : string;
+      releaseDate : string;
+      description : string;
+      price : number;
+      starRating : number;
+      imageUrl : string;
+       
+    }
 
