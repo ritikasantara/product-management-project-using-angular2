b87cfa7bb0c36a2aef4063176a4fa45ccc92d42a
ritikasantara <rits.santara@gmail.com>
2017-04-18 18:38:35 +0530

    created product service which stores product info

--- pm-angular2/src/app/components/products/product-list/product-list.component.ts
+++ pm-angular2/src/app/components/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -17,7 +18,7 @@ export class ProductListComponent implements OnInit {
   showImage: boolean;
   listFilter: string;
 
-  constructor() { 
+  constructor(private productService: ProductService) { 
 
     this.pageTitle = "Product List";
     this.imageWidth = 50;
@@ -26,55 +27,7 @@ export class ProductListComponent implements OnInit {
     this.productTdpadding = 12;
     this.listFilter = "Bag";
 
-    this.products = [
-      {
-        "productId" : 1,
-        "productName" : "Louis Vuitton Tote Bag",
-        "productCode" : "abc-123",
-        "releaseDate" : "March 18, 2017",
-        "description" : "Tote Bag....lorem ipsom h",
-        "price" : 1099,
-        "starRating" : 2.4,
-        "imageUrl" : "http://uk.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-armand-open-tote-taurillon-leather-men-s-bags--M42683_PM2_Front%20view.jpg"
-      },
-
-      {
-        "productId" : 2,
-        "productName" : "Polyvore Satchel Bag",
-        "productCode" : "pqr-456",
-        "releaseDate" : "April 07, 2017",
-        "description" : "Satchel Bag....lorem ipsom h",
-        "price" : 1599,
-        "starRating" : 4.6,
-        "imageUrl" : "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=161416907"
-
-      },
-
-      {
-
-        "productId" : 3,
-        "productName" : "Butterflies Sling Bag",
-        "productCode" : "ghi-789",
-        "releaseDate" : "Feb 25, 2017",
-        "description" : "Sling Bag....lorem ipsom h",
-        "price" : 1089,
-        "starRating" : 5,
-        "imageUrl" : "https://n1.sdlcdn.com/imgs/a/0/p/Butterflies-Quilted-Beige-Cross-Body-SDL888771080-1-6ae5d.jpg"
-      },
-
-      {
-
-        "productId" : 4,
-        "productName" : "Hidesign Pouch Bag",
-        "productCode" : "def-678",
-        "releaseDate" : "March 28, 2017",
-        "description": "Tote Bag....lorem ipsom h",
-        "price" : 1299,
-        "starRating" : 3.5,
-        "imageUrl" : "http://www.bodenimages.com/productimages/productmedium/17wsum_am278_tan.jpg"
-      }
-
-    ];
+  
 
  }
 
@@ -85,12 +38,17 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void{
+
       console.log('Product-list OnInit initiated..');
+      this.products = this.productService.getProducts();
+    
+
     }
 
     onRatingClicked(message: string): void{
 
       this.pageTitle = 'Product List: ' +message;
+
     }
 
 
--- pm-angular2/src/app/components/star/star.component.ts
+++ pm-angular2/src/app/components/star/star.component.ts
@@ -17,7 +17,7 @@ export class StarComponent implements OnChanges {
   }
 
   onClick(): void {
-    this.ratingClicked.emit('The rating ${this.rating} was clicked!');
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
   }
 
 }
